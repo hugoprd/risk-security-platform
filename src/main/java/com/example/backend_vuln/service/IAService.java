76976a1b709beba969f81a7bb77fa14bf7709d06
jvpmacedo <101368896+jvpmacedo@users.noreply.github.com>
2025-11-19 17:41:14 -0300

    Create IAService.java

---
+++ src/main/java/com/example/backend_vuln/service/IAService.java
@@ -0,0 +1,24 @@
+package com.example.backend_vuln.service;
+
+import com.example.backend_vuln.controller.LlmAgentClient;
+import com.example.backend_vuln.model.Vulnerabilidade;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IAService {
+
+    public String gerarRecomendacao(Vulnerabilidade vul) {
+        // Monta o texto
+        String textoParaIA = "Analise a vulnerabilidade: " + vul.getTitulo() + 
+                             ". Descrição: " + vul.getDescricao() + 
+                             ". CVSS: " + vul.getPontuacao_cvss();
+
+        try {
+            // Chama o código do Hugo
+            LlmAgentClient clienteHugo = new LlmAgentClient();
+            return clienteHugo.getSuggestion(textoParaIA);
+        } catch (Exception e) {
+            return "Erro na IA: " + e.getMessage();
+        }
+    }
+}
