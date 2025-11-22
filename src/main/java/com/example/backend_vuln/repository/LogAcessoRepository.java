87b9456ef3191439aaf135025b6e6bc0a17420bf
hugo <hugo.prd.br@gmail.com>
2025-11-22 13:53:32 -0300

    Adiciona repositório mongo no LogAcesso

--- src/main/java/com/example/backend_vuln/repository/LogAcessoRepository.java
+++ src/main/java/com/example/backend_vuln/repository/LogAcessoRepository.java
@@ -1,7 +1,7 @@
 package com.example.backend_vuln.repository;
 
 import com.example.backend_vuln.model.LogAcesso;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
