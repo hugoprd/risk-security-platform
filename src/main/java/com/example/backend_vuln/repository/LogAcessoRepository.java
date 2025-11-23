40fa43b54903bf88b33667e58312e167993942d3
jvpmacedo <101368896+jvpmacedo@users.noreply.github.com>
2025-11-23 19:13:26 -0300

    Create LogAcessoRepository.java

---
+++ src/main/java/com/example/backend_vuln/repository/LogAcessoRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend_vuln.repository;
+
+import com.example.backend_vuln.model.LogAcesso;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LogAcessoRepository extends MongoRepository<LogAcesso, String> {
+}
